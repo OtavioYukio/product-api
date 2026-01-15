bb29faab410fd0b5312bd8aa8833826999be996f
OtavioYukio <yukiootavio@gmail.com>
2026-01-15 14:46:05 -0300

    feat: criar metodo getAll no ProductController

---
+++ src/main/java/com/otavioyukio/product_api/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.otavioyukio.product_api.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.otavioyukio.product_api.model.ProductResponseDTO;
+import com.otavioyukio.product_api.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getAll() {
+        List<ProductResponseDTO> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    } 
+
+}
