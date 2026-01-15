1e74beaf5d0ae5a8b93fd338efe3d4b9b64ef7a3
OtavioYukio <yukiootavio@gmail.com>
2026-01-15 13:30:03 -0300

    feat: criar inteface ProductService

---
+++ src/main/java/com/otavioyukio/product_api/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.otavioyukio.product_api.service;
+
+import java.util.List;
+
+import com.otavioyukio.product_api.model.ProductRequestDTO;
+import com.otavioyukio.product_api.model.ProductResponseDTO;
+
+public interface ProductService {
+    List<ProductResponseDTO> getAllProducts();
+    ProductResponseDTO getProductById(Long id);
+    ProductResponseDTO createProduct(ProductRequestDTO productRequestDTO);
+    ProductRequestDTO updateProduct(ProductRequestDTO productRequestDTO);
+    void deleteProductById(Long id);
+}
