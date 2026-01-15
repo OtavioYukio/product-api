3f24164bc710d058358469f19a3c79664a613575
OtavioYukio <yukiootavio@gmail.com>
2026-01-15 13:54:41 -0300

    feat: criar classe de exceção ProductNotFoundException

---
+++ src/main/java/com/otavioyukio/product_api/exception/ProductNotFoundException.java
@@ -0,0 +1,13 @@
+package com.otavioyukio.product_api.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    
+    public ProductNotFoundException(Long id) {
+        super("Product with ID " + id + " not found.");
+    } 
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+
+}
